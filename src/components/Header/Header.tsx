f1599df305ca6929769b3e5682a913f6dcd84053
Augusto <augustoinacio243@gmail.com>
2026-08-09 23:28:49 -0300

    feat: enhance Header component layout with improved title styling and adjust button alignment

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -21,12 +21,11 @@ export default function Header() {
   return (
     <>
       <IonHeader style={{backgroundColor: "#fff", boxShadow: "none", position: "sticky", top: 0}}>
-        <header className="m-2 row justify-content-space between aling-items-center">
-          <div className="col-12 col-md-4">
-            <h1>RepoHound</h1>
+        <header className="m-2 row justify-content-md-between justify-content-between aling-items-center">
+          <div className="col-7 col-sm-6 col-md-6">
+            <h1 style={{fontWeight: "bolder", fontSize: "2rem"}}><span style={{fontStyle: "italic", color: "#01214a"}}>Repo</span><span style={{fontStyle: "italic", color: "#ef6d0f"}}>Hound</span></h1>
           </div>
-          <div className="col-12 col-md-4"></div>
-          <div className="col-12 col-md-4 d-flex justify-content-md-end justify-content-start align-items-center p-0">
+          <div className="col-5 col-sm-6 col-md-6 d-flex justify-content-sm-end justify-content-md-end justify-content-end align-items-center p-0 gap-2">
             <Button variant="outline" color="#eb670d" className="ms-3" onClick={() => window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSeiPH1Wk-WrD97xQqvhRMLn2ch3tNcSOOw7z4WxqY9am8j8yg/viewform?usp=dialog'} radius={"md"}>
               Avalie
             </Button>
